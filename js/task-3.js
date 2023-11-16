2e7ce0d78dfc9bdb89de70782edcf9651eac4fb5
Mariia69 <141348185+Mariia69@users.noreply.github.com>
2023-11-16 19:29:59 +0200

    update task-2.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,34 @@
-
-
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+class StringBuilder {
+    constructor(initialValue) {
+      this._value = initialValue;
+    }
+  
+    getValue() {
+      return this._value;
+    }
+  
+    padStart(str) {
+      this._value = str + this._value;
+    }
+  
+    padEnd(str) {
+      this._value += str;
+    }
+  
+    padBoth(str) {
+      this.padStart(str);
+      this.padEnd(str);
+    }
+  }
+  
+  const builder = new StringBuilder(".");
+  console.log(builder.getValue()); // "."
+  
+  builder.padStart("^");
+  console.log(builder.getValue()); // "^."
+  
+  builder.padEnd("^");
+  console.log(builder.getValue()); // "^.^"
+  
+  builder.padBoth("=");
+  console.log(builder.getValue()); // "=^.^="
